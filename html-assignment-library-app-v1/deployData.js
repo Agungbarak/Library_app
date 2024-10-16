d1eb9e0345c0cafd79785a1defda4645d94b436b
agungbarak <agungbarak20@gmail.com>
2024-10-17 00:03:58 +0700

    Initial commit

---
+++ html-assignment-library-app-v1/deployData.js
@@ -0,0 +1,15 @@
+/* 
+  buatlah beberapa variable untuk pengecekan deployment
+
+  studentName: berisi string nama student
+  studentId: berisi string CAMPID
+  NetlifyDeployUrl: berisi string url Netlify deployment
+*/
+
+const studentName = 'agungmubarok'; // TODO: replace this
+
+const studentId = 'fs11217827'; // TODO: replace this
+
+const NetlifyDeployUrl = 'https://fs11217827-agungmubarok.netlify.app'; // TODO: replace this
+
+module.exports = { studentName, studentId, NetlifyDeployUrl };
